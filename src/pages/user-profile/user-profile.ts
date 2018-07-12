de78c95222f9e1410bbc0a6c8183815f5438d46d
Guillermo Garcia <guillermo.garcia@ciberexperis.es>
2018-07-12 18:00:59 +0200

    creada page user profile

---
+++ src/pages/user-profile/user-profile.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-user-profile',
+  templateUrl: 'user-profile.html'
+})
+export class UserProfile {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
